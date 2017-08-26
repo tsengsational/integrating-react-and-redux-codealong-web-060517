fa7a947e9f01b7c5e2fdceb388541cd0b455f9c4
github username <github email address>
2017-08-26 00:28:43 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
+import Counter from './components/Counter';
 
 class App extends Component {
   render() {
     return (
-      <div className="App" >
-        Counter Component here.
+      <div className="App">
+        <Counter store={this.props.store} />
       </div>
     );
-  }
-}
+  };
+};
 
 export default App;
